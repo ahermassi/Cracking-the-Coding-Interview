fa0ccc76f2a05d3e75d02ee7b8c60883c857ed70
Anouer Hermassi <ahermass@stevens.edu>
2019-06-09 22:21:07 +0100

    Added O anlaysis

--- arrays_strings/is_unique_chars.py
+++ arrays_strings/is_unique_chars.py
@@ -1,9 +1,16 @@
-def is_unique_chars(s):
-    chars = list(s)
+def is_unique_chars_v1(str):
+    """
+    Let N be the string length
+    List creation: O(N)
+    Set creation: O(N)
+    len function: O(1)
+    => O(N)
+    """
+    chars = list(str)
     unique_chars = set(chars)
     return len(chars) == len(unique_chars)
 
 
 if __name__ == '__main__':
     s = "Hello"
-    print(is_unique_chars(s))
+    print(is_unique_chars_v1(s))
